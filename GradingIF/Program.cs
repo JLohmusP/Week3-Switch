9406aaf0e373d611301e13a250b6019a20f52d82
Jaak Valter Lõhmuspuu <jlohmusp@tktk.ee>
2021-10-17 14:21:49 +0300

    add new projects

---
+++ GradingIF/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace GradingIF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //küsi hinnet;
+            //kui "A", kuva "Suurepärane!";
+            //Kui "B", kuva "Väga hea!";
+            //Kui "C", kuva "Hea!";
+            //Kui "D", kuva "Rahuldav.";
+            //Kui "E", kuva "Kasin.";
+            //Kui "F", kuva "Puudulik.";
+            //Kui midagi muud, kuva "Vale väärtus.";
+
+
+            Console.WriteLine("Sisesta oma tulemus:");
+            char userResult = Convert.ToChar(Console.ReadLine().ToUpper());
+
+            if (userResult == 'A')
+            {
+                Console.WriteLine("Suurepärane!");
+            }
+            else if (userResult == 'B'){
+                Console.WriteLine("Väga hea!");
+            }
+            else if (userResult == 'C')
+            {
+                Console.WriteLine("Hea!");
+            }
+            else if (userResult == 'D')
+            {
+                Console.WriteLine("Rahuldav.");
+            }
+            else if (userResult == 'E')
+            {
+                Console.WriteLine("Kasin.");
+            }
+            else if (userResult == 'F')
+            {
+                Console.WriteLine("Puudulik.");
+            }
+            else
+            {
+                Console.WriteLine("Vale väärtus.");
+            }
+        }
+    }
+}
---
+++ PersonalityTest/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace PersonalityTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Küsi kasutaja lemmikvärv;
+            // Kui "punane", kuva "oled romantiline";
+            //Kui "sinine", kuva "oled töökas";
+            //Kui "roheline", kuva "oled looduse sõber";
+            //Kui midagi muud, kuva "oled {userColor} ükssarik";
+
+            Console.WriteLine("Sisesta oma lemmikvärv:");
+            string userColor = Console.ReadLine();
+
+            if(userColor == "punane")
+            {
+                Console.WriteLine("Oled romantiline.");
+            }
+            else if(userColor == "sinine"){
+                Console.WriteLine("Oled töökas.");
+            }
+            else if(userColor == "roheline")
+            {
+                Console.WriteLine("Oled looduse sõber.");
+            }
+            else
+            {
+                Console.WriteLine($"Oled {userColor} ükssarvik.");
+            }
+
+            Console.WriteLine("Kena päeva");
+        }
+    }
+}
